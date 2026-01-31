57a05f7ed9998fc01e77054908f6c9d08bc357ec
Kevin <verquerre.kevin@gmail.com>
2026-01-31 20:21:31 +0100

    feat(models): add maintenance period field and property to UserDietmetricsProfile

--- dietmetrics/dietmetrics/models/user_profile.py
+++ dietmetrics/dietmetrics/models/user_profile.py
@@ -5,6 +5,8 @@ Docstring for dietmetrics.models
 from django.db import models
 from django.contrib.auth import get_user_model
 
+from ..conf import DietmetricsSettings
+
 UserModel = get_user_model()
 
 
@@ -22,6 +24,27 @@ class UserDietmetricsProfile(models.Model):
         related_name="dietmetrics_profile",
     )
 
+    # Settings fields
+
+    maintenance_period_days = models.IntegerField(
+        null=True,
+        blank=True
+    )
+
+    @property
+    def maintenance_period(self) -> int:
+        """
+        Return the user's maintenance period in days.
+
+        This value indicates the number of days over which maintenance calories
+        are calculated or averaged.
+        """
+        return (
+            self.maintenance_period_days
+            if self.maintenance_period_days is not None
+            else DietmetricsSettings.MAINTENANCE_PERIOD_DAYS
+        )
+
     @property
     def maintenance_calories(self) -> int:
         """
@@ -31,3 +54,14 @@ class UserDietmetricsProfile(models.Model):
         required to maintain the current body weight.
         """
         return 0
+
+    class Meta: # pylint: disable=too-few-public-methods
+        """
+        Meta class to add constraints.
+        """
+        constraints = [
+            models.CheckConstraint(
+                condition=models.Q(maintenance_period_days__gt=0),
+                name="maintenance_period_positive"
+            )
+        ]
