f1367abc9371585acfbe524cbec9009abaad8750
Kevin <verquerre.kevin@gmail.com>
2026-02-01 13:31:32 +0100

    feat(models): enhance DayMetric model with additional fields and constraints

--- dietmetrics/dietmetrics/models/day_metrics.py
+++ dietmetrics/dietmetrics/models/day_metrics.py
@@ -20,6 +20,46 @@ class DayMetric(models.Model):
         on_delete=models.CASCADE,
         related_name="daily_metrics",
     )
-    date = models.DateField()
-    calories = models.IntegerField()
-    activity_expenditure = models.IntegerField()
+    date = models.DateField(
+        null=False,
+        blank=False
+    )
+    add_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+    calories = models.IntegerField(
+        null=True,
+        blank=True
+    )
+    activity_expenditure = models.IntegerField(
+        null=True,
+        blank=True
+    )
+    weight = models.DecimalField(
+        null=True,
+        blank=True,
+        max_digits=5,
+        decimal_places=2
+    )
+
+    class Meta: # pylint: disable=too-few-public-methods
+        """
+        Docstring for Meta class to add constraints.
+        """
+        constraints = [
+            models.UniqueConstraint(
+                fields=["dietmetrics_user", "date"],
+                name="unique_user_date_metric"
+            ),
+            models.CheckConstraint(
+                condition=models.Q(calories__gt=0),
+                name="calories_positive"
+            ),
+            models.CheckConstraint(
+                condition=models.Q(weight__gt=0),
+                name="weight_positive"
+            ),
+            models.CheckConstraint(
+                condition=models.Q(activity_expenditure__gte=0),
+                name="activity_expenditure_non_negative"
+            )
+        ]
